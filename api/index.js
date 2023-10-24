a8e44b536a2a452af8f21f0a19f1e0340768d89f
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-10-24 12:11:36 -0500

    Changed the way the server starts

--- api/index.js
+++ api/index.js
@@ -2,12 +2,6 @@ import server from "./src/app.js";
 import { conn,PORT } from "./src/db.js";
 
 //*Syncing all the models at once.
-// conn.sync({ force: true }).then(() => {
-//   server.listen(3001, () => {
-//     console.log("%s listening at 3001"); // eslint-disable-line no-console
-//   });
-// });
-// const PORT= 3001
 const startServer = async () => {
   try {
     await conn.sync({ force: true });

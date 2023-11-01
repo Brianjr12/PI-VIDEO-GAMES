8bea3f53c70ab90ac1284aa0232acd1c861cb2cd
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-11-01 04:42:57 -0500

    Routes were created for all controllers

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
-import { videoGamesById, videoGamesByName } from '../controllers/testController.js';
+import { allVideoGames} from '../controllers/videoGames.js';
 import {createVideoGame} from '../controllers/newVideogame.js'
+import { genres } from '../controllers/getGenres.js';
+import { videoGamesById } from '../controllers/videoGameById.js';
+import { videoGamesByName } from '../controllers/videoGameByName.js';
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -9,8 +12,10 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-// router.get("/videogames/:idVideogame", videoGamesById);
+router.get("/genres", genres )
+router.get("/videogames",allVideoGames)
 router.get("/videogames/name",videoGamesByName)
+router.get("/videogames/:idVideogame", videoGamesById);
 router.post("/videogames",createVideoGame);
 
 

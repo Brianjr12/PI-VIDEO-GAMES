b1f3b2cf66a691a38f4fac84b8edeae1c3cc1838
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-11-02 03:06:32 -0500

    routes were added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
+import {videoGames} from './api/VideoGamesApi.js'
+import {Routes,Route} from 'react-router-dom';
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import DetailPage from './pages/DetailPage'
+import CreateGamePage from './pages/CreateGamePage'
+import NotFoundPage from './pages/NotFoundPage'
 
-function App() {
+function  App(){
 
   return (
     <>
-      <h1>hello world</h1>
+      <Routes>
+        <Route  path='/' element={<LoginPage/>} />
+        <Route  path='/home' element={<HomePage videoGames={videoGames}  />} />
+        <Route  path='/detail' element={<DetailPage />} />
+        <Route  path='/create' element={<CreateGamePage />} />
+        <Route  path='*' element={<NotFoundPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App

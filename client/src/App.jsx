c13dbf887e4e10f77b795d94b1ff07d91bdd1c69
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-11-04 03:55:15 -0500

    Added path for page that shows cards by name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,25 @@
-import './App.css'
-import {videoGames} from './api/VideoGamesApi.js'
-import {Routes,Route} from 'react-router-dom';
-import LoginPage from './pages/LoginPage'
-import HomePage from './pages/HomePage'
-import DetailPage from './pages/DetailPage'
-import CreateGamePage from './pages/CreateGamePage'
-import NotFoundPage from './pages/NotFoundPage'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/Home/HomePage.jsx";
+import DetailPage from "./pages/DetailPage";
+import CreateGamePage from "./pages/CreateGamePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import GamesNamePage from "./pages/GamesNamePage.jsx";
 
-function  App(){
-
+function App() {
   return (
     <>
       <Routes>
-        <Route  path='/' element={<LoginPage/>} />
-        <Route  path='/home' element={<HomePage videoGames={videoGames}  />} />
-        <Route  path='/detail' element={<DetailPage />} />
-        <Route  path='/create' element={<CreateGamePage />} />
-        <Route  path='*' element={<NotFoundPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/name" element={<GamesNamePage />} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path="/create" element={<CreateGamePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
 }
 
-export default App
+export default App;

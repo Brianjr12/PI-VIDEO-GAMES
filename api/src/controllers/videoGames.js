9becc7510c1919ed75645e5e5d32b60ad32abb49
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-11-04 03:57:22 -0500

    Fixed a bug when submitting games

--- api/src/controllers/videoGames.js
+++ api/src/controllers/videoGames.js
@@ -6,8 +6,10 @@ const { API_KEY } = process.env;
 
 export const allVideoGames = async (req, res) => {
   try {
+    let page = req.query.page || 1; // Get the page number of the query, or use page 1 if not specified
+
     // search for video games in the database
-    const videoGamesFromDB = async () => {
+    const videoGamesFromDB = async (startIndex, endIndex) => {
       try {
         const { Videogame, Genre } = models;
         const videoGameDB = await Videogame.findAll({
@@ -19,8 +21,8 @@ export const allVideoGames = async (req, res) => {
           ],
         });
 
-        const filterVideoGameDB = videoGameDB.map(
-          ({ id, name, platforms, image, released, rating, Genres }) => ({
+        const filterVideoGameDB = videoGameDB
+          .map(({ id, name, platforms, image, released, rating, Genres }) => ({
             id,
             name,
             image,
@@ -28,8 +30,8 @@ export const allVideoGames = async (req, res) => {
             rating,
             genres: Genres.map(({ name }) => name),
             platforms,
-          })
-        );
+          }))
+          .slice(startIndex, endIndex);
         return filterVideoGameDB;
       } catch (error) {
         res.status(500).json({ error: error.message });
@@ -38,64 +40,99 @@ export const allVideoGames = async (req, res) => {
 
     const videoGamesDB = await videoGamesFromDB();
 
+    // send the video games from the database
+    const sendVideoGamesDB = async (page) => {
+      try {
+        let startIndex = 0;
+        let endIndex = 15;
+        let pages = 1;
+
+        while (startIndex < videoGamesDB.length) {
+          if (page === pages) {
+            const videoGamesDBs = await videoGamesFromDB(startIndex, endIndex);
+            return videoGamesDBs;
+          }
+          pages++;
+          startIndex += 15;
+          endIndex += 15;
+        }
+        return [];
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+    // send video games from the API and database to the page
+    const sendVideoGames = async (page) => {
+      try {
+        const videoGamesDbs = await sendVideoGamesDB(page);
+        //fusionar juegos con juegos API si hay menos de 15 en la base de datos
+        if (videoGamesDbs.length > 0 && videoGamesDbs.length < 15) {
+          const allVideoGames = [...videoGamesDbs, ...videoGamesAPI].slice(
+            0,
+            15
+          );
+          return res.status(200).json(allVideoGames);
+        }
+        // send 10 video games to page 7
+        if (videoGamesDbs.length === 0) {
+          if (page === 7) {
+            return res.status(200).json(videoGamesAPI.slice(0, 10));
+          }
+          return res.status(200).json(videoGamesAPI);
+        }
+        if (videoGamesDbs.length > 0 && page === 7)
+          return res.status(200).json(videoGamesDbs.slice(0, 10));
+        // send video games from the database to the pages
+        return res.status(200).json(videoGamesDbs);
+      } catch (error) {
+        return res.status(500).json({ error: error.message });
+      }
+    };
+
     // search for video games in the API
-    const maxPageSize = 100 - videoGamesDB.length; // sets the maximum page size allowed for the API
-    const requestedPageSize = 20; // page size you want in your interface
-    const pageSize = Math.min(requestedPageSize, maxPageSize); // Choose the smallest page size between the requested and the maximum allowed
-
-    const page = req.query.page || 1; // Get the page number of the query, or use page 1 if not specified
-
-    const startIndex = (page - 1) * pageSize;
-    const endIndex = page * pageSize;
-
     const videoGamesFromAPI = async () => {
       try {
+        const maxPageSize = 100 - videoGamesDB.length; // sets the maximum page size allowed for the API
+        const requestedPageSize = 15;
+        const pageSize = Math.min(requestedPageSize, maxPageSize); // Choose the smallest page size between the requested and the maximum allowed
+
         const URL = `https://api.rawg.io/api/games?key=${API_KEY}&page_size=${pageSize}&page=${page}`;
         const { data } = await axios.get(URL);
-        const videoGameAPI = data.results
-          .slice(startIndex, endIndex)
-          .map(
-            ({
-              id,
-              name,
-              slug,
-              background_image,
-              released,
-              rating,
-              genres,
-              parent_platforms,
-            }) => ({
-              id,
-              name,
-              slug,
-              image: background_image,
-              released,
-              rating,
-              genres: genres.map(({ name }) => name),
-              platforms: parent_platforms.map(({ platform }) => platform.name),
-            })
-          );
+        const videoGameAPI = data.results.map(
+          ({
+            id,
+            name,
+            slug,
+            background_image,
+            released,
+            rating,
+            genres,
+            parent_platforms,
+          }) => ({
+            id,
+            name,
+            slug,
+            image: background_image,
+            released,
+            rating,
+            genres: genres.map(({ name }) => name),
+            platforms: parent_platforms.map(({ platform }) => platform.name),
+          })
+        );
         return videoGameAPI;
       } catch (error) {
         res.status(500).json({ error: error.message });
       }
     };
 
+    // validate if database is empty
     const videoGamesAPI = await videoGamesFromAPI();
-
-    // An array was created with the game database and the API
-    const combinedVideos = [...videoGamesDB, ...videoGamesAPI];
-    // The matrix was limited to not exceed 20 video games
-    const limitedVideoGames = combinedVideos.slice(0, 20);
-
-    // console.log(
-    //   `esta es la pagina ${page} y trae: ${limitedVideoGames.length} juegos`
-    // );
-
-    res.status(200).json(limitedVideoGames);
+    if (videoGamesDB.length === 0) {
+      return res.status(200).json(videoGamesAPI);
+    }
+    // execute the assigned function to send the video games
+    sendVideoGames(Number(page));
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
-
-

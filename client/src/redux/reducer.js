a247a1ce89e536db464439f33edbb56e6953b73a
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-11-05 23:30:25 -0500

    reducers were created for actions

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,21 +1,46 @@
-import {SAVE_GAMES,CHANGE_PAGE,GAME_BY_NAME} from './actionType.js'
+import {
+  SAVE_GAMES,
+  CHANGE_PAGE,
+  GAMES_BY_NAME,
+  GAME_BY_ID,
+  FILTER_ORIGIN_BY_DB,FILTER_ORIGIN_BY_API,
+  SAVE_GENRES,FILTER_BY_GENRES,SORT_ASCENDING,SORT_BY_RATING,SORT_DESCENDING
+} from "./actionType.js";
 const initialState = {
   games: [],
-  gamesByName: [],
-  page:1
-}
+  gameById: {},
+  genres: [],
+  limit: 0,
+  page: 1,
+};
 
-const reducer = (state = initialState,{type,payload}) => {
+const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case SAVE_GAMES:
-      return { ...state, games: payload }
+      return { ...state, games: payload.games, limit: payload.limit };
+    case SAVE_GENRES:
+      return { ...state, genres: payload };
     case CHANGE_PAGE:
-      return { ...state, games: payload.data, page: payload.page };
-    case GAME_BY_NAME:
-      return { ...state, games: payload};
+      return { ...state, games: payload.games, page: payload.page };
+    case GAMES_BY_NAME:
+      return { ...state, games: payload };
+    case GAME_BY_ID:
+      return { ...state, gameById: payload };
+    case FILTER_ORIGIN_BY_DB:
+      return { ...state, games: payload };
+    case FILTER_ORIGIN_BY_API:
+      return { ...state, games: payload };
+    case FILTER_BY_GENRES:
+      return { ...state, games: payload };
+    case SORT_ASCENDING:
+      return { ...state, games: payload }
+    case SORT_DESCENDING:
+      return { ...state, games: payload }
+    case SORT_BY_RATING:
+      return { ...state, games: payload }
     default:
-      return {...state};
+      return { ...state };
   }
-}
+};
 
-export default reducer
+export default reducer;

6c6095d0960a04aa3b9ac2b9baedf1763c16acdf
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-11-05 23:34:36 -0500

    The routes were conditioned for the header and navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+// hooks
+import { Routes, Route, useLocation } from "react-router-dom";
+// components
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/Home/HomePage.jsx";
+import NavBar from "./components/navBar/NavBar.jsx";
+import Header from './components/header/Header.jsx'
 import DetailPage from "./pages/DetailPage";
 import CreateGamePage from "./pages/CreateGamePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import GamesNamePage from "./pages/GamesNamePage.jsx";
 
 function App() {
+  const { pathname } = useLocation();
   return (
     <>
+      {pathname !== "/" && (<><NavBar/> <Header/></>)}
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />

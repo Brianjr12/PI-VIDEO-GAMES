50d044ff8a58ec8ac98366af63ed5ba5f7b401b2
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-11-06 00:09:46 -0500

    added page limit

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -5,6 +5,8 @@ import { changePage } from "../../redux/actions";
 const Pagination = () => {
   const dispatch = useDispatch();
   const page = useSelector(({ page }) => page);
+  const limit = useSelector(({ limit }) => limit);
+
   const nextPage = () => {
     dispatch(changePage(page + 1));
   };
@@ -15,8 +17,8 @@ const Pagination = () => {
   return (
     <div>
       {page > 1 && <button onClick={prevPage}> {"< Prev"}</button>}
-      {page < 7 && <button onClick={nextPage}>{"Next >"}</button>}
-      <h1>Page: {page}</h1>
+      { page < (limit / 15) && <button onClick={nextPage}>{"Next >"}</button>}
+      <p>Page: {page}</p>
     </div>
   );
 };

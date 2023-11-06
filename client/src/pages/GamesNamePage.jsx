417bdc1b40379d1c222c8fc8f2f82185beaae8a0
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-11-06 00:15:50 -0500

    there are no changes

--- client/src/pages/GamesNamePage.jsx
+++ client/src/pages/GamesNamePage.jsx
@@ -3,7 +3,6 @@ import Cards from '../components/cards/Cards'
 import NavBar from '../components/navBar/NavBar';
 const GamesNamePage = () => {
   const gamesByName = useSelector(({ gamesByName }) => gamesByName);
-  console.log("names", gamesByName);
   return (
     <div>
       <NavBar/>

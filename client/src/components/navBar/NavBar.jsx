aa6900d5e168d323267aec9e92705e0c73065942
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-11-06 00:08:26 -0500

    the rendering was conditioned

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,10 +1,31 @@
-import SearchBar from "../searchBar/SearchBar"
+import SearchBar from "../searchBar/SearchBar";
+import { Link, useLocation } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
+  const { pathname } = useLocation();
+
   return (
-    <div>
-      <SearchBar/>
+    <div className="nav-container">
+      <nav>
+        {pathname !== "/home" && (
+          <Link to="/home" className="nav-link">
+            Home
+          </Link>
+        )}
+
+        {pathname !== "/create" && (
+          <Link to="/create" className="nav-link">
+            Create a game
+          </Link>
+        )}
+        <Link to="/" className="nav-logout">
+          Exit
+        </Link>
+        <SearchBar />
+      </nav>
     </div>
-  )
-}
-export default NavBar
+  );
+};
+
+export default NavBar;

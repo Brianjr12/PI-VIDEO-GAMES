9fe45dfd72e318e744df0494f0616ffba0074bb2
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-11-06 00:19:39 -0500

    The genres are saved at the time of assembling the component

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -2,21 +2,19 @@ import { useEffect, useState } from "react";
 import Cards from "../../components/cards/Cards.jsx";
 import './HomePage.css'
 import { useDispatch, useSelector } from 'react-redux'
-import {saveGames} from '../../redux/actions.js'
+import {saveGames,saveGenres} from '../../redux/actions.js'
 import Pagination from "../../components/pagination/Pagination.jsx";
-import NavBar from "../../components/navBar/NavBar.jsx";
 
 const HomePage = () => {
   const videoGames = useSelector(({games}) => games)
   const dispatch = useDispatch();
-console.log("games",videoGames);
   useEffect(() => {
     dispatch(saveGames())
+    dispatch(saveGenres())
   },[dispatch]);
 
   return (
     <div id="home" className="container-home">
-      <NavBar/>
       <Pagination/>
       <Cards games={videoGames}/>
     </div>
